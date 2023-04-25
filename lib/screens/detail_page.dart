373ec8c5a3cd9cec0c47b702c69e23c5b51738b6
TanMar23 <nicte.tania@gmail.com>
2023-04-25 11:02:44 -0600

    small refactor

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../models/character.dart';
 import '../provider/character_provider.dart';
+import '../widgets/circle_avatar_img.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({
@@ -175,83 +176,6 @@ TableRow getTableRow({
       ),
     ],
   );
-}
-
-class CircleAvatarImg extends StatelessWidget {
-  const CircleAvatarImg({
-    super.key,
-    required this.img,
-    required this.status,
-  });
-
-  final String img;
-  final String status;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.center,
-      children: [
-        CircleAvatar(
-          backgroundColor: getStatusColor(status: status),
-          radius: 114,
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(img),
-            radius: 110,
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 4,
-              horizontal: 8,
-            ),
-            decoration: BoxDecoration(
-              color: getStatusColor(status: status),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(8),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                )
-              ],
-            ),
-            child: Center(
-                child: Text(
-              status.toUpperCase(),
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-              ),
-            )),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-Color getStatusColor({required String status}) {
-  Color colorStatus;
-  switch (status) {
-    case 'Alive':
-      colorStatus = const Color(0xFF55CB44);
-      break;
-    case 'Dead':
-      colorStatus = Colors.red;
-      break;
-    case 'unknown':
-      colorStatus = const Color(0xFFFF9800);
-      break;
-    default:
-      colorStatus = Colors.black;
-  }
-  return colorStatus;
 }
 
 IconData getGenderIcon({required String gender}) {
---
+++ lib/widgets/circle_avatar_img.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class CircleAvatarImg extends StatelessWidget {
+  const CircleAvatarImg({
+    super.key,
+    required this.img,
+    required this.status,
+  });
+
+  final String img;
+  final String status;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: getStatusColor(status: status),
+          radius: 114,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(img),
+            radius: 110,
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 8,
+            ),
+            decoration: BoxDecoration(
+              color: getStatusColor(status: status),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(8),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                )
+              ],
+            ),
+            child: Center(
+                child: Text(
+              status.toUpperCase(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+              ),
+            )),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Color getStatusColor({required String status}) {
+  Color colorStatus;
+  switch (status) {
+    case 'Alive':
+      colorStatus = const Color(0xFF55CB44);
+      break;
+    case 'Dead':
+      colorStatus = Colors.red;
+      break;
+    case 'unknown':
+      colorStatus = const Color(0xFFFF9800);
+      break;
+    default:
+      colorStatus = Colors.black;
+  }
+  return colorStatus;
+}
